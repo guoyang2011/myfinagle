fea64a90420e666ae532a3f294905e7a2a0dad8a
guoyang2011 <guoyang2011@gmail.com>
2015-01-05 18:03:09 +0800

    add finagle thrift cluster test

--- ThriftDemo/src/main/scala/cn/changhong/thrift/Start.scala
+++ ThriftDemo/src/main/scala/cn/changhong/thrift/Start.scala
@@ -11,31 +11,35 @@ object Start {
   val zkHost="localhost:2181"
   val zkPath="/thrift_1"
   def main(args:Array[String]): Unit ={
-    runServer(startZkService)
+    (1 to 10).foreach{index=>runServer(new InetSocketAddress(index+10000),startZkService)}
     Thread.sleep(1000)
     runServer(startZkClient)
   }
-  def startZkService: Unit ={
+  def startZkService(bind:InetSocketAddress): Unit ={
     val zkResolver=new ZkResolver()
     val zkAnn=new ZkAnnouncer()
     val zkAddr=zkHost+"!"+zkPath+"!0"
-    val bind=new InetSocketAddress(10000)
-    ThriftServer(bind,"thrift_service")
+    ThriftServer(bind,"thrift_service"+bind.getPort)
     zkResolver.bind(zkAddr)
     zkAnn.announce(bind,zkAddr)
   }
   def startZkClient: Unit ={
     val client=ThriftClient("zk!"+zkHost+"!"+zkPath)
-    (1 to 10).foreach{index=>
+    (1 to 100).foreach{index=>
       client.deleteArtificaillyNes(1) onSuccess{rep=>
-        println(rep)
+//        println(rep)
       } onFailure{ex=>ex.fillInStackTrace()}
-      Thread.sleep(1000)
+      Thread.sleep(100)
     }
   }
   def runServer(fn: =>Unit): Unit ={
     new Thread(new Runnable {
       override def run(): Unit = fn
+    }).start()
+  }
+  def runServer(bind:InetSocketAddress,fn: (InetSocketAddress)=>Unit): Unit ={
+    new Thread(new Runnable {
+      override def run(): Unit = fn(bind)
     }).start()
   }
 }
--- ThriftDemo/src/main/scala/cn/changhong/thrift/ThriftServer.scala
+++ ThriftDemo/src/main/scala/cn/changhong/thrift/ThriftServer.scala
@@ -12,7 +12,7 @@ import org.apache.thrift.protocol.TBinaryProtocol
 /**
  * Created by yangguo on 15-1-5.
  */
-object IndexNewsOperationImp extends IndexNewsOperatorServices.FutureIface{
+class IndexNewsOperationImp(name:String) extends IndexNewsOperatorServices.FutureIface{
   val count=new AtomicInteger(0)
   override def indexNews(indexNews: NewsModel): Future[Boolean] = Future.value{
     if(count.addAndGet(1)%2==0) true
@@ -20,13 +20,14 @@ object IndexNewsOperationImp extends IndexNewsOperatorServices.FutureIface{
   }
 
   override def deleteArtificaillyNes(id: Int): Future[Int] = Future.value{
+    println(name)
     if(count.getAndAdd(1)%3==0) 0
     else 1
   }
 }
 object ThriftServer {
   def apply(addr:InetSocketAddress,name:String): Unit ={
-    val service =new IndexNewsOperatorServices.FinagledService(IndexNewsOperationImp,new TBinaryProtocol.Factory())
+    val service =new IndexNewsOperatorServices.FinagledService(new IndexNewsOperationImp(name),new TBinaryProtocol.Factory())
     ServerBuilder()
       .bindTo(addr)
       .codec(ThriftServerFramedCodec())
